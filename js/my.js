cbda4bcaccedafaa1a6d103011eedbeeced9afdd
Usb233 <807564472@qq.com>
2015-10-05 14:13:03 +0800

    js ~

--- js/my.js
+++ js/my.js
@@ -66,5 +66,24 @@ $(document).ready(function() {
 		$(".length").val(parseInt(1.7 * Math.sqrt($(".power").val())));
 	});
 
+ //平台、设备和操作系统
+        var system = {
+            win: false,
+            mac: false,
+            xll: false,
+            ipad:false
+        };
+        //检测平台
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+        system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+        //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+        if (system.win || system.mac || system.xll||system.ipad) {}
+        else {
+            $(".img-title").css("top","-53px")
+        };
+
 });
 
