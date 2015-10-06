18aaee3a2c7bbc7cddd1f0e86a663a84068e4fa7
Usb233 <807564472@qq.com>
2015-10-06 22:34:14 +0800

    bug

--- js/my.js
+++ js/my.js
@@ -80,22 +80,21 @@ $(document).ready(function() {
       });
 
 
-
-//检测平台、设备和操作系统
+/*检测平台、设备和操作系统*/
 	var system = {
 		win: false,
 		mac: false,
 		xll: false,
 		ipad: false
 	};
-	//检测平台
+	/*检测平台*/
 	var p = navigator.platform;
 	system.win = p.indexOf("Win") == 0;
 	system.mac = p.indexOf("Mac") == 0;
 	system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
 	system.ipad = (navigator.userAgent.match(/iPad/i) != null) ? true : false;
-	//跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
 	if (system.win || system.mac || system.xll || system.ipad) {} else {
+		/*Android优化*/
 		$(".img-title").css("top", "-55px")
 	};
 
