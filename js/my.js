e2b9f59924a216d1bd4a875755dfbaa9920ffab6
Usb233 <807564472@qq.com>
2015-10-12 15:33:04 +0800

    555

--- js/my.js
+++ js/my.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	/*顶栏*/
+/*顶栏*/
 	$(document).bind("scroll", function() {
 		if ($(window).scrollTop() <= 210) {
 			$(".menu").css({
@@ -15,7 +15,7 @@ $(document).ready(function() {
 		}
 	});
     
-    /*顶栏超链接*/
+/*顶栏超链接*/
 	$(".menu>li:eq(0)>a").css({
 		"border-bottom": "solid #fff 3px"
 	});
@@ -39,6 +39,24 @@ $(document).ready(function() {
 		});
 		$(".card").fadeIn(100);
 	});
+
+/*卡片开关*/
+  var a=0;
+  $(".toggle").slideUp(0);
+  $(".show-ic").click(function(){
+    a++; if (a>2) {a=1};
+  	switch (a){
+  	case 1:
+  	$(this).css("transform","rotate(180deg)");
+  	$(this).next().slideDown(350,'easeInOutQuart');
+  	break;
+  	case 2:
+  	$(this).css("transform","rotate(0deg)");
+    $(this).next().slideUp(350,'easeInOutSine');
+  	break;
+    } 	
+  });
+
 
 });
 
