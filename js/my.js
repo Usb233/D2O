b1372d9155a5f829e7bc313bbacc457cd2b79e09
Usb233 <807564472@qq.com>
2015-11-15 13:43:58 +0800

    +local storage

--- js/my.js
+++ js/my.js
@@ -16,12 +16,12 @@ $(document).ready(function() {
 });
 
 /*顶栏超链接*/
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$(".about").fadeOut(0);
 	$(".menu>li:eq(0)>a").css({
 		"border-bottom": "solid #fff 3px"
 	});
-	/*关于*/
+	
 	$(".menu>li:eq(1)>a").click(function() {
 			$(this).css({
 				"border-bottom": "solid #fff 3px"
@@ -32,7 +32,7 @@ $(document).ready(function() {
 			$(".card").fadeOut(100);
 			setTimeout(function(){$(".about").fadeIn(250)},100)
 		})
-		/*首页*/
+	
 	$(".menu>li:eq(0)>a").click(function() {
 		$(this).css({
 			"border-bottom": "solid #fff 3px"
@@ -43,7 +43,7 @@ $(document).ready(function() {
 		$(".card").fadeIn(100);
 		$(".about").fadeOut(100);
 	});
-});
+});*/
 
 /*主页*/
 /*$(document).ready(function() {
@@ -114,21 +114,42 @@ $(document).ready(function() {
 			$(".show-ic").css("background-color", "#fff")
 		}, 600)
 	});
-
 });
-/*lp计算*/
+
+/*闭包*/
+function clearLocalStorage(){
+	localStorage.clear();
+}
+
+
+/*LP计算*/
 $(document).ready(function() {
+	if (localStorage.lp_l) {$(".length").val(localStorage.lp_l)}
+	if (localStorage.lp_p) {$(".power").val(localStorage.lp_p)}
+	
 	$(".length").change(function() {
 		$(".power").val(parseInt(($(".length").val() / 1.7) * ($(".length").val() / 1.7)));
+	    localStorage.lp_l=$(".length").val();
+	    localStorage.lp_p=$(".power").val();
 	});
 
 	$(".power").change(function() {
 		$(".length").val(parseInt(1.7 * Math.sqrt($(".power").val())));
+	    localStorage.lp_l=$(".length").val();
+	    localStorage.lp_p=$(".power").val();
 	});
 });
 
-/*zc计算*/
+/*ZC计算*/
 $(document).ready(function() {
+	if (localStorage.zc_fl==60) {}
+	else {
+	  /*if (localStorage.zc_fl) {$(":checked").val(localStorage.zc_fl)}*/
+	  if (localStorage.zc_e) {$("form:eq(1)").find("input:eq(2)").val(localStorage.zc_e)}
+      if (localStorage.zc_i) {$("form:eq(1)").find("input:eq(3)").val(localStorage.zc_i)}
+      if (localStorage.zc_rc) {$("form:eq(1)").find("input:eq(4)").val(localStorage.zc_rc)}
+	}
+    
 	$("form:eq(1)").change(function() {
 		var FL = $(":checked").val();
 		var E = $("form:eq(1)").find("input:eq(2)").val();
@@ -136,17 +157,28 @@ $(document).ready(function() {
 		var Z = E / I;
 		var C = 1000000 / (6.2832 * Z * FL);
 		$("form:eq(1)").find("input:eq(4)").val(C.toFixed(4));
+		localStorage.zc_fl = $(":checked").val();
+        localStorage.zc_e = $("form:eq(1)").find("input:eq(2)").val();
+        localStorage.zc_i = $("form:eq(1)").find("input:eq(3)").val();
+        localStorage.zc_rc = $("form:eq(1)").find("input:eq(4)").val();
 	});
 });
 
 /*LC计算*/
 $(document).ready(function() {
+	if (localStorage.lc_l) {$(".l").val(localStorage.lc_l)}
+	if (localStorage.lc_c) {$(".c").val(localStorage.lc_c)}
+	if (localStorage.lc_f) {$(".f").val(localStorage.lc_f)}
+
 	$(".l").change(function() {
 		var l = $(".l").val();
 		var c = $(".c").val();
 		var f = $(".f").val();
 		var f_re = 1 / (6.28318 * Math.sqrt(l * c));
 		$(".f").val(f_re.toFixed(3));
+        localStorage.lc_l=$(".l").val();
+	    localStorage.lc_c=$(".c").val();
+	    localStorage.lc_f=$(".f").val();
 	});
 	$(".c").change(function() {
 		var l = $(".l").val();
@@ -154,11 +186,22 @@ $(document).ready(function() {
 		var f = $(".f").val();
 		var f_re = 1 / (6.28318 * Math.sqrt(l * c));
 		$(".f").val(f_re.toFixed(3));
+        localStorage.lc_l=$(".l").val();
+	    localStorage.lc_c=$(".c").val();
+	    localStorage.lc_f=$(".f").val();
 	});
 });
 
 /*NC计算*/
 $(document).ready(function() {
+	if (localStorage.nc_ic) {$(".iC").val(localStorage.nc_ic)}
+	if (localStorage.nc_iv) {$(".iV").val(localStorage.nc_iv)}
+	if (localStorage.nc_ix) {$(".iX").val(localStorage.nc_ix)}
+	if (localStorage.nc_iy) {$(".iY").val(localStorage.nc_iy)}
+	if (localStorage.nc_cNum) {$(".C-num").val(localStorage.nc_cNum)}
+	if (localStorage.nc_reC) {$(".re-C").val(localStorage.nc_reC)}
+    if (localStorage.nc_reV) {$(".re-V").val(localStorage.nc_reV)}
+
 	$(".NC>form>.toggle>input").change(function() {
 		iC = $(".iC").val();
 		iV = $(".iV").val();
@@ -166,24 +209,44 @@ $(document).ready(function() {
 		iY = $(".iY").val();
 		$(".C-num").val(iX * iY);  
 		$(".re-C").val( ( (iC/iX)*iY ).toFixed(4) );
-		$(".re-V").val( iV*iX ) ;
-
+		$(".re-V").val( iV*iX );
+        localStorage.nc_ic = iC;
+        localStorage.nc_iv = iV;
+        localStorage.nc_ix = iX;
+        localStorage.nc_iy = iY;
+        localStorage.nc_cNum = $(".C-num").val();
+        localStorage.nc_reC = $(".re-C").val();
+        localStorage.nc_reV = $(".re-V").val();
     });
 });
 
 /*CC计算*/
 $(document).ready(function() {
-	$("div").click(
-	function(){
+    if (localStorage.cc_un) {$(".UN").children(":selected").val(localStorage.cc_un)}
+    if (localStorage.cc_dc) {$(".DC").children(":selected").val(localStorage.cc_dc)}
+    if (localStorage.cc_n) {$(".N").val(localStorage.cc_n)}
+    if (localStorage.cc_d) {$(".D").val(localStorage.cc_d)}
+    if (localStorage.cc_l) {$(".L").val(localStorage.cc_l)}
+    if (localStorage.cc_w) {$(".W").val(localStorage.cc_w)}
+    if (localStorage.cc_c) {$(".C").val(localStorage.cc_c)}
+
+	$(".CC>form>.toggle>input").change(function(){
 	u = $(".UN").children(":selected").val()
 	d = $(".D").val() * u
-	a = ($(".L").val() * u) * (($(".L").val() * u))
+	a = ($(".L").val() * u) * (($(".W").val() * u))
 	n = $(".N").val()
 	dc = $(".DC").children(":selected").val()
 	c = ((8.85e-12 * dc * a) / d) * (n - 1)
 	c *= 1e+12
 	c = parseInt(c) / 1e+6
 	$(".C").val(c)
+    localStorage.cc_un = u
+    localStorage.cc_dc = dc
+    localStorage.cc_n = n
+    localStorage.cc_d = $(".D").val()
+    localStorage.cc_l = $(".L").val()
+    localStorage.cc_w = $(".W").val()
+    localStorage.cc_c = $(".C").val()
   })
 });
 
