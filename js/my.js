ae93bc4eed15bb9a0a8906c75bedcd6607a1b6c1
Usb233 <807564472@qq.com>
2015-12-13 14:21:05 +0800

    2bug

--- js/my.js
+++ js/my.js
@@ -5,45 +5,56 @@ $(document).ready(function() {
 	/*清除localStorage*/
 	function clearLocalStorage() {
 		localStorage.clear();
-	} 
+	}
 });
 
 /*顶栏动画*/
 $(document).ready(function() {
-    $(document).bind("scroll", function() {
-    	var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	$(document).bind("scroll", function() {
+		var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 		var top = $(window).scrollTop();
 		console.log(top)
 		var topH = $(".top").css("height"); /*top高度*/
 		var top_h = topH.substring(0, 2);
 		var top_shadow = 210 - top_h;
 
-     if (winWidth >= 380) {
-		if ($(window).scrollTop() >= top_shadow || $("#back").css("display")=="block") {
-			$(".top").css({
-				"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
-				/*"top": "210px",*/
-			})
-		}
-		else{
-			$(".top").css({
-				"box-shadow": "",
-				/*"top": "210px",*/
-			})
-		}
-		h1Size = (3/2500)*top*top-(11/50)*top+40;  /*60/(top/60)*/
-		h1Top = (-9/20)*top+50;
-		if (h1Size<=26) {h1Size=26};
-		if (h1Size>=40) {h1Size=40};
-		if (top>=100) {h1Size=26};
-		/*var winWidth0 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-		if (winWidth0<=480) {if(h1Top<=7) {h1Top=7};}
-		else {if(h1Top<=11) {h1Top=11};}*/
-		if(h1Top<=11) {h1Top=11};
+		if (winWidth >= 380) {
+			if ($(window).scrollTop() >= top_shadow || $("#back").css("display") == "block") {
+				$(".top").css({
+					"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
+					/*"top": "210px",*/
+				})
+			} else {
+				$(".top").css({
+					"box-shadow": "",
+					/*"top": "210px",*/
+				})
+			}
+			h1Size = (3 / 2500) * top * top - (11 / 50) * top + 40; /*60/(top/60)*/
+			h1Top = (-9 / 20) * top + 50;
+			if (h1Size <= 26) {
+				h1Size = 26
+			};
+			if (h1Size >= 40) {
+				h1Size = 40
+			};
+			if (top >= 100) {
+				h1Size = 26
+			};
+			/*var winWidth0 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+			if (winWidth0<=480) {if(h1Top<=7) {h1Top=7};}
+			else {if(h1Top<=11) {h1Top=11};}*/
+			if (h1Top <= 11) {
+				h1Top = 11
+			};
 
-        $("h1").css({"font-size":h1Size+"px"});
-        $("h1").css({"top":h1Top+"px"});
-    };
+			$("h1").css({
+				"font-size": h1Size + "px"
+			});
+			$("h1").css({
+				"top": h1Top + "px"
+			});
+		};
 	});
 });
 
@@ -58,14 +69,17 @@ $(document).ready(function() {
 		$("#back").show();
 		$("#moreIc").hide();
 
-		if (top<=10) {$(".top-bg").animate({
-			height: '60px'
-		}, 450, 'easeInOutCubic')}
-		else{$(".top-bg").animate({
-			height: '60px'
-		}, 10)}
-		
-		if ( $("#back").css("display")=="block") {
+		if (top <= 10) {
+			$(".top-bg").animate({
+				height: '60px'
+			}, 450, 'easeInOutCubic')
+		} else {
+			$(".top-bg").animate({
+				height: '60px'
+			}, 10)
+		}
+
+		if ($("#back").css("display") == "block") {
 			$(".top").css({
 				"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
 			})
@@ -73,21 +87,28 @@ $(document).ready(function() {
 	});
 
 	/*退出第二界面*/
-	$("#back").click(function () {
+	$("#back").click(function() {
 		$(".card").show();
 		$("h1").fadeIn(400);
 		$("#back").hide();
 		$("#moreIc").show();
 		var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 		if (winWidth >= 380) {
-		$(".top-bg").animate({
-			height: '210px'
-		}, 450, 'easeInOutCubic');}
+			$(".top-bg").animate({
+				height: '210px'
+			}, 450, 'easeInOutCubic');
+		}
+		else{
+			$(".top").css({
+				"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
+			})
+		}
 
-		if ($("#back").css("display")=="none") {
+		if ($("#back").css("display") == "none") {
 			$(".top").css({
 				"box-shadow": "",
-			})};
+			})
+		};
 
 		var top = $(window).scrollTop();
 		console.log(top)
@@ -95,7 +116,7 @@ $(document).ready(function() {
 		var top_h = topH.substring(0, 2);
 		var top_shadow = 210 - top_h;
 
-		if ($(window).scrollTop() >= top_shadow || $("#back").css("display")=="block") {
+		if ($(window).scrollTop() >= top_shadow || $("#back").css("display") == "block") {
 			$(".top").css({
 				"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
 				/*"top": "210px",*/
@@ -106,28 +127,27 @@ $(document).ready(function() {
 
 /*顶栏选项*/
 $(document).ready(function() {
-    $("#moreList").hide();
-    $("#blank").hide();
+	$("#moreList").hide();
+	$("#blank").hide();
 
-    $("#moreIc").click(function(){
-        $("#moreList").show();
-        $("#blank").show();
-    });
-    $("#moreList").find("*").click(function(){
-        $("#moreList").hide();
-        $("#blank").hide();
-    });
+	$("#moreIc").click(function() {
+		$("#moreList").show();
+		$("#blank").show();
+	});
+	$("#moreList").find("*").click(function() {
+		$("#moreList").hide();
+		$("#blank").hide();
+	});
 
-    $("#blank").mousedown(function() {
-    	$(this).hide();
-    	$("#moreList").hide();
-    });
+	$("#blank").mousedown(function() {
+		$(this).hide();
+		$("#moreList").hide();
+	});
 
-    $(document).bind("scroll", function() {
-        $("#blank").hide();
-    	$("#moreList").hide();
-    });
-
+	$(document).bind("scroll", function() {
+		$("#blank").hide();
+		$("#moreList").hide();
+	});
 });
 
 /*主页*/
@@ -152,8 +172,14 @@ $(document).ready(function() {
 			$(this).data("lastClick", "1");
 		} else {
 			/*alert("不同");*/
-			if ($(this).data("cardSwitch") == "close") {a++};
-			if ($(this).data("cardSwitch") == "open"){if (a>1) {a--};}
+			if ($(this).data("cardSwitch") == "close") {
+				a++
+			};
+			if ($(this).data("cardSwitch") == "open") {
+				if (a > 1) {
+					a--
+				};
+			}
 			$(".show-ic").removeData("lastClick");
 			$(this).data("lastClick", "1");
 		}
@@ -162,7 +188,7 @@ $(document).ready(function() {
 		a++;
 		if (a > 2) {
 			a = 1
-		};	
+		};
 		switch (a) {
 			/*(this为.show-ic)*/
 			case 1:
@@ -203,32 +229,41 @@ $(document).ready(function() {
 
 /*LP计算*/
 $(document).ready(function() {
-	if (localStorage.lp_l) {$(".length").val(localStorage.lp_l)}
-	if (localStorage.lp_p) {$(".power").val(localStorage.lp_p)}
-	
+	if (localStorage.lp_l) {
+		$(".length").val(localStorage.lp_l)
+	}
+	if (localStorage.lp_p) {
+		$(".power").val(localStorage.lp_p)
+	}
+
 	$(".length").change(function() {
 		$(".power").val(parseInt(($(".length").val() / 1.7) * ($(".length").val() / 1.7)));
-	    localStorage.lp_l=$(".length").val();
-	    localStorage.lp_p=$(".power").val();
+		localStorage.lp_l = $(".length").val();
+		localStorage.lp_p = $(".power").val();
 	});
 
 	$(".power").change(function() {
 		$(".length").val(parseInt(1.7 * Math.sqrt($(".power").val())));
-	    localStorage.lp_l=$(".length").val();
-	    localStorage.lp_p=$(".power").val();
+		localStorage.lp_l = $(".length").val();
+		localStorage.lp_p = $(".power").val();
 	});
 });
 
 /*ZC计算*/
 $(document).ready(function() {
-	if (localStorage.zc_fl==60) {}
-	else {
-	  /*if (localStorage.zc_fl) {$(":checked").val(localStorage.zc_fl)}*/
-	  if (localStorage.zc_e) {$("form:eq(1)").find("input:eq(2)").val(localStorage.zc_e)}
-      if (localStorage.zc_i) {$("form:eq(1)").find("input:eq(3)").val(localStorage.zc_i)}
-      if (localStorage.zc_rc) {$("form:eq(1)").find("input:eq(4)").val(localStorage.zc_rc)}
+	if (localStorage.zc_fl == 60) {} else {
+		/*if (localStorage.zc_fl) {$(":checked").val(localStorage.zc_fl)}*/
+		if (localStorage.zc_e) {
+			$("form:eq(1)").find("input:eq(2)").val(localStorage.zc_e)
+		}
+		if (localStorage.zc_i) {
+			$("form:eq(1)").find("input:eq(3)").val(localStorage.zc_i)
+		}
+		if (localStorage.zc_rc) {
+			$("form:eq(1)").find("input:eq(4)").val(localStorage.zc_rc)
+		}
 	}
-    
+
 	$("form:eq(1)").change(function() {
 		var FL = $(":checked").val();
 		var E = $("form:eq(1)").find("input:eq(2)").val();
@@ -237,17 +272,23 @@ $(document).ready(function() {
 		var C = 1000000 / (6.2832 * Z * FL);
 		$("form:eq(1)").find("input:eq(4)").val(C.toFixed(4));
 		localStorage.zc_fl = $(":checked").val();
-        localStorage.zc_e = $("form:eq(1)").find("input:eq(2)").val();
-        localStorage.zc_i = $("form:eq(1)").find("input:eq(3)").val();
-        localStorage.zc_rc = $("form:eq(1)").find("input:eq(4)").val();
+		localStorage.zc_e = $("form:eq(1)").find("input:eq(2)").val();
+		localStorage.zc_i = $("form:eq(1)").find("input:eq(3)").val();
+		localStorage.zc_rc = $("form:eq(1)").find("input:eq(4)").val();
 	});
 });
 
 /*LC计算*/
 $(document).ready(function() {
-	if (localStorage.lc_l) {$(".l").val(localStorage.lc_l)}
-	if (localStorage.lc_c) {$(".c").val(localStorage.lc_c)}
-	if (localStorage.lc_f) {$(".f").val(localStorage.lc_f)}
+	if (localStorage.lc_l) {
+		$(".l").val(localStorage.lc_l)
+	}
+	if (localStorage.lc_c) {
+		$(".c").val(localStorage.lc_c)
+	}
+	if (localStorage.lc_f) {
+		$(".f").val(localStorage.lc_f)
+	}
 
 	$(".l").change(function() {
 		var l = $(".l").val();
@@ -255,9 +296,9 @@ $(document).ready(function() {
 		var f = $(".f").val();
 		var f_re = 1 / (6.28318 * Math.sqrt(l * c));
 		$(".f").val(f_re.toFixed(3));
-        localStorage.lc_l=$(".l").val();
-	    localStorage.lc_c=$(".c").val();
-	    localStorage.lc_f=$(".f").val();
+		localStorage.lc_l = $(".l").val();
+		localStorage.lc_c = $(".c").val();
+		localStorage.lc_f = $(".f").val();
 	});
 	$(".c").change(function() {
 		var l = $(".l").val();
@@ -265,68 +306,96 @@ $(document).ready(function() {
 		var f = $(".f").val();
 		var f_re = 1 / (6.28318 * Math.sqrt(l * c));
 		$(".f").val(f_re.toFixed(3));
-        localStorage.lc_l=$(".l").val();
-	    localStorage.lc_c=$(".c").val();
-	    localStorage.lc_f=$(".f").val();
+		localStorage.lc_l = $(".l").val();
+		localStorage.lc_c = $(".c").val();
+		localStorage.lc_f = $(".f").val();
 	});
 });
 
 /*NC计算*/
 $(document).ready(function() {
-	if (localStorage.nc_ic) {$(".iC").val(localStorage.nc_ic)}
-	if (localStorage.nc_iv) {$(".iV").val(localStorage.nc_iv)}
-	if (localStorage.nc_ix) {$(".iX").val(localStorage.nc_ix)}
-	if (localStorage.nc_iy) {$(".iY").val(localStorage.nc_iy)}
-	if (localStorage.nc_cNum) {$(".C-num").val(localStorage.nc_cNum)}
-	if (localStorage.nc_reC) {$(".re-C").val(localStorage.nc_reC)}
-    if (localStorage.nc_reV) {$(".re-V").val(localStorage.nc_reV)}
+	if (localStorage.nc_ic) {
+		$(".iC").val(localStorage.nc_ic)
+	}
+	if (localStorage.nc_iv) {
+		$(".iV").val(localStorage.nc_iv)
+	}
+	if (localStorage.nc_ix) {
+		$(".iX").val(localStorage.nc_ix)
+	}
+	if (localStorage.nc_iy) {
+		$(".iY").val(localStorage.nc_iy)
+	}
+	if (localStorage.nc_cNum) {
+		$(".C-num").val(localStorage.nc_cNum)
+	}
+	if (localStorage.nc_reC) {
+		$(".re-C").val(localStorage.nc_reC)
+	}
+	if (localStorage.nc_reV) {
+		$(".re-V").val(localStorage.nc_reV)
+	}
 
 	$(".NC>form>.toggle>input").change(function() {
 		iC = $(".iC").val();
 		iV = $(".iV").val();
 		iX = $(".iX").val();
 		iY = $(".iY").val();
-		$(".C-num").val(iX * iY);  
-		$(".re-C").val( ( (iC/iX)*iY ).toFixed(4) );
-		$(".re-V").val( iV*iX );
-        localStorage.nc_ic = iC;
-        localStorage.nc_iv = iV;
-        localStorage.nc_ix = iX;
-        localStorage.nc_iy = iY;
-        localStorage.nc_cNum = $(".C-num").val();
-        localStorage.nc_reC = $(".re-C").val();
-        localStorage.nc_reV = $(".re-V").val();
-    });
+		$(".C-num").val(iX * iY);
+		$(".re-C").val(((iC / iX) * iY).toFixed(4));
+		$(".re-V").val(iV * iX);
+		localStorage.nc_ic = iC;
+		localStorage.nc_iv = iV;
+		localStorage.nc_ix = iX;
+		localStorage.nc_iy = iY;
+		localStorage.nc_cNum = $(".C-num").val();
+		localStorage.nc_reC = $(".re-C").val();
+		localStorage.nc_reV = $(".re-V").val();
+	});
 });
 
 /*CC计算*/
 $(document).ready(function() {
-    if (localStorage.cc_un) {$(".UN").children(":selected").val(localStorage.cc_un)}
-    if (localStorage.cc_dc) {$(".DC").children(":selected").val(localStorage.cc_dc)}
-    if (localStorage.cc_n) {$(".N").val(localStorage.cc_n)}
-    if (localStorage.cc_d) {$(".D").val(localStorage.cc_d)}
-    if (localStorage.cc_l) {$(".L").val(localStorage.cc_l)}
-    if (localStorage.cc_w) {$(".W").val(localStorage.cc_w)}
-    if (localStorage.cc_c) {$(".C").val(localStorage.cc_c)}
+	if (localStorage.cc_un) {
+		$(".UN").children(":selected").val(localStorage.cc_un)
+	}
+	if (localStorage.cc_dc) {
+		$(".DC").children(":selected").val(localStorage.cc_dc)
+	}
+	if (localStorage.cc_n) {
+		$(".N").val(localStorage.cc_n)
+	}
+	if (localStorage.cc_d) {
+		$(".D").val(localStorage.cc_d)
+	}
+	if (localStorage.cc_l) {
+		$(".L").val(localStorage.cc_l)
+	}
+	if (localStorage.cc_w) {
+		$(".W").val(localStorage.cc_w)
+	}
+	if (localStorage.cc_c) {
+		$(".C").val(localStorage.cc_c)
+	}
 
-	$(".CC>form>.toggle>input").change(function(){
-	u = $(".UN").children(":selected").val()
-	d = $(".D").val() * u
-	a = ($(".L").val() * u) * (($(".W").val() * u))
-	n = $(".N").val()
-	dc = $(".DC").children(":selected").val()
-	c = ((8.85e-12 * dc * a) / d) * (n - 1)
-	c *= 1e+12
-	c = parseInt(c) / 1e+6
-	$(".C").val(c)
-    localStorage.cc_un = u
-    localStorage.cc_dc = dc
-    localStorage.cc_n = n
-    localStorage.cc_d = $(".D").val()
-    localStorage.cc_l = $(".L").val()
-    localStorage.cc_w = $(".W").val()
-    localStorage.cc_c = $(".C").val()
-  })
+	$(".CC>form>.toggle>input").change(function() {
+		u = $(".UN").children(":selected").val()
+		d = $(".D").val() * u
+		a = ($(".L").val() * u) * (($(".W").val() * u))
+		n = $(".N").val()
+		dc = $(".DC").children(":selected").val()
+		c = ((8.85e-12 * dc * a) / d) * (n - 1)
+		c *= 1e+12
+		c = parseInt(c) / 1e+6
+		$(".C").val(c)
+		localStorage.cc_un = u
+		localStorage.cc_dc = dc
+		localStorage.cc_n = n
+		localStorage.cc_d = $(".D").val()
+		localStorage.cc_l = $(".L").val()
+		localStorage.cc_w = $(".W").val()
+		localStorage.cc_c = $(".C").val()
+	})
 });
 
 /*检测平台、设备和操作系统*/
@@ -345,7 +414,7 @@ $(document).ready(function() {
 	if (system.win || system.mac || system.xll || system.ipad) {} else {
 		//Android优化
 		/*$(".img-title").css("top", "-53px");*/
-		$("h1").css("font-size", "40px")
+		/*$("h1").css("font-size", "40px")*/
 	};
 });
 
@@ -381,21 +450,21 @@ $(document).ready(function() {
 	var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	console.log(winWidth);
 
-		if (winWidth <= 380) {
-			$(".top-bg").css({
-				"height": "60px",
-			});
-            $(".top").css({
-				"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
-			});
-			$("h1").css({
-				"font-size": "26px"
-			});
-			$("h1").css({
-				"top": "11px"
-			});
-			$(".LP").css({
-				"margin-top": "50px"
-			});
-		};
+	if (winWidth <= 380) {
+		$(".top-bg").css({
+			"height": "60px",
+		});
+		$(".top").css({
+			"box-shadow": "0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 5px 0 rgba(0,0,0,0.23)",
+		});
+		$("h1").css({
+			"font-size": "26px"
+		});
+		$("h1").css({
+			"top": "11px"
+		});
+		$(".LP").css({
+			"margin-top": "50px"
+		});
+	};
 });
